df6ad83a178dba2d0316664292bd14fb28399e5f
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-06-26 09:13:05 -0300

    feat: busca as referências pelo app

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,13 @@
+import { logger } from "./utils/logger.js";
+
 //Controllers
+import ProductController from "./controllers/ProductController.js";
 
 export default abstract class App {
-  static start(): void {
+  static async start(): Promise<void> {
     // Busca referências dos produtos para cadastro
+    const manufacturers = await ProductController.getManufacturers();
+    logger.info("Manufacturers fetched: " + manufacturers.join(", "));
     // Busca os produtos no Magento
     // Pega os atributos dos produtos
     // Adiciona os atributos dos produtos no Magento
