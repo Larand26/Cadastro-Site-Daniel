00bd078f329cab0e54246b94761dba52a13371d3
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-06-26 08:46:41 -0300

    refactro: cria o fluxo de cadastro de produtos no Magento

---
+++ src/App.ts
@@ -0,0 +1,8 @@
+export default abstract class App {
+  static start(): void {
+    // Busca referências dos produtos para cadastro
+    // Busca os produtos no Magento
+    // Pega os atributos dos produtos
+    // Adiciona os atributos dos produtos no Magento
+  }
+}
